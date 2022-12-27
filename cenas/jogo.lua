53ef4b471c97d5c82149949100c0ad11b523c1d0
matheusssilva991 <matheus.ssilva991@gmail.com>
2022-12-27 19:05:25 -0300

    'Update: Time'

--- cenas/jogo.lua
+++ cenas/jogo.lua
@@ -5,18 +5,25 @@ function Jogo:new()
     require "cenas/fase2"
     require "cenas/fase3"
 
+    id_jogador = 1
     nivel_fase = 1
     trocou_fase = false
-
+    tempo_jogo = 0
     fase = Fase1()
+    tabela_ranking = {}
 end
 
 function Jogo:update(dt)
     fase:update(dt)
+    tempo_jogo = tempo_jogo + dt
 
     if fase.estado == 'finalizado' then
-        nivel_fase = nivel_fase + 1
-        trocou_fase = true
+        if nivel_fase < 3 then
+            nivel_fase = nivel_fase + 1
+            trocou_fase = true
+        else
+            table.insert(tabela_ranking, {nome='Jogador' .. id_jogador, tempo_jogo=tonumber(string.format("%.2f", tempo_jogo))})
+        end
     end
 
     if nivel_fase == 2 and trocou_fase then
@@ -33,8 +40,8 @@ end
 function Jogo:draw()
     fase:draw()
     love.graphics.setColor(0, 0, 0)
-    love.graphics.print("estado: " .. fase.estado, 10, 10)
-    love.graphics.print("nivel fase: " .. nivel_fase, 10, 40)
+    --love.graphics.print("tempo jogo: " .. math.floor(tempo_jogo), 10, 10)
+    love.graphics.print("tempo jogo: " .. tonumber(string.format("%.2f", tempo_jogo)), 10, 10)
     love.graphics.setColor(1, 1, 1)
 end
 
